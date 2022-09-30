d9a75aa1aebff40bdd1bc7a511e43be4d6389b64
simeonsimeon <lingakumardae@gmail.com>
2022-09-30 12:09:47 -0600

    add

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
-
 let btn=document.getElementById("btn");
-var obj={};
-var count=0;
+let done=document.getElementById("done");
+
 function rg(easy1){
     return parseInt(Math.random()*easy1);
 }
@@ -13,28 +12,55 @@ function symbol(easy){
             return "-"
         case "multi":
             return "*"
+            case "divide":
+            return "/"
     }
 }
+
+
+
 var whole=document.createElement("div");
     whole.className+="whole";
     document.body.appendChild(whole);
 function create(num1,num2,operation){
     var divchange=document.createElement("div");
     divchange.className+="divchange";
+    divchange.setAttribute("class","div")
     var h1=document.createElement("h2");
+    h1.setAttribute("class","no1")
     h1.innerHTML=num1;
     var h2=document.createElement("h2");
+    h2.setAttribute("class","operation")
     h2.innerHTML=symbol(operation)
     var h3=document.createElement("h2");
+    h3.setAttribute("class","no2")
     h3.innerHTML=num2;
-    var input=document.createElement("input");
-    input.setAttribute("class","answer");
-    input.setAttribute("type","txt");
+   var input=document.createElement("input");
+    input.setAttribute("type","answer")
+   input.setAttribute("class","answer");
+    // input.setAttribute("class","answer");
+    // input.setAttribute("type","txt");
     divchange.appendChild(h1);
     divchange.appendChild(h2);
     divchange.appendChild(h3);
     divchange.appendChild(input);
-    whole.appendChild(divchange)
+    whole.appendChild(divchange);
+    ha=input.value;
+    
+    bg=divchange;
+    // ldf(ha)
+}
+
+
+
+
+function store(raw1,raw2,op){
+
+    if(op=="add"){
+      add=raw1+raw2;
+      arr.push(add);
+     
+    }
 }
     function click(){
         var range1=document.getElementById("range").value;
@@ -42,44 +68,67 @@ function create(num1,num2,operation){
         var range=parseInt(range1);
         var random1;
         var random2;
-        for(let i=0;i<=8;i++){
+        if(operation!=="divide"){
+        for(let i=0;i<range;i++){
              random1=rg(range);
              random2=rg(range);
              create(random2,random1,operation);
-             
-            
-     }
+              
+            }
+        }else{
+            for(let i=0;i<range;i++){
+                random1=rg(range);
+                random2=rg(range);
+                if((random1%random2==0)&&(random1&&random2!=0)&&(random2<=random1)){
+                    create(random1,random2,operation);
+                }
+               }
+        }
+        }
      btn.addEventListener("click",click);
+   function stage(){
+          let you=document.getElementsByClassName("div"),promblem,operationelm,numb1,numb2,operationtype;
+let no1value,no2value,act,actvalue,a=0;
+
+
+          
+          for(let i=0;i<you.length;i++){
+                  promblem=you[i];
+                  
+                  operationelm=promblem.getElementsByClassName("operation")[0];
+                  operationtype=operationelm.innerHTML;
+
+                  numb1=promblem.getElementsByClassName("no1")[0];
+                  no1value=parseInt(numb1.innerHTML);
+                  numb2=promblem.getElementsByClassName("no2")[0];
+                  no2value=parseInt(numb2.innerHTML);
+                  if(operationtype=="+"){
+                      var answer=no1value+no2value;
+                    //   console.log(answer)
+                  }else if(operationtype=="-"){
+                    var answer=no1value-no2value;
+                  }else if(operationtype=="*"){
+                    var answer=no1value*no2value;
+                  }
+
+
+                  act=promblem.getElementsByClassName("answer")[0];
+                  console.log(act);
+                  actvalue=act.value;
+                  console.log(actvalue);
+                
+                  if(answer==actvalue){
+                      a++
+                  }
+
+                
 
 
 
-
-// let a=parseInt(prompt("Give Number"));
-// if(a>0){
-//     console.log("this is Positive Number");
-// }else if(a<0){
-//     console.log("this is  negative Number");
-// }else{
-//     console.log("No Negative and Positive");
-// }
-
-
-// let a="simeon";
-// let b="";
-// for(let i=a.length;i>0;i--){
-//     console.log(a[i-1]);
-//     b+=a[i-1];
-// }
-// console.log(b);
-
-
-
-// let a="ammaa";
-// let b=a.split("").reverse().join("");
-// if(a==b){
-//     console.log("palindrome");
-// }else{
-//     console.log("no Palindrome");
-// }
-
+          }
+          alert(a)
+   }
+   done.addEventListener("click",function(){
+    stage()
+   })
 
